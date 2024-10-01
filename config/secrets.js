f76d14d52f6a71b3d250c8f70c04b66c52574d34
Yash Sethia <56500374+yash-sethia@users.noreply.github.com>
2024-10-01 23:23:32 +0530

    Update secrets.js

--- config/secrets.js
+++ config/secrets.js
@@ -1,5 +1,5 @@
 
 module.exports = {
     token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://yash:qwerty123@csai-website.pm1ro.mongodb.net/?retryWrites=true&w=majority&appName=csai-website"
+    mongo_connection : "mongodb+srv://myAtlasDBUser:NLbsMBZUYu4JopMM@myatlasclusteredu.3faoaf3.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU"
 };
